6c4d3373a5c43be3d3fd9149274611feb085d58a
dbhide <dhanashree.bhide3@gmail.com>
2020-04-09 18:48:58 +0530

    ADDED: Password Validation

--- src/main/java/com/myFirstServlet/LoginServlet.java
+++ src/main/java/com/myFirstServlet/LoginServlet.java
@@ -2,7 +2,6 @@ package com.myFirstServlet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,19 +17,22 @@ import java.io.PrintWriter;
 public class LoginServlet extends HttpServlet {
 
     static String VALID_USERNAME = "^[A-Z]{1}[a-z]{2,}$";
+    static String VALID_PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}$";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userName = request.getParameter("userName");
         String userPassword = request.getParameter("userPassword");
 
-        if (userName.matches(VALID_USERNAME) && userPassword.equals("Dhanashree")) {
+        boolean validName = (userName != null) && userName.matches(VALID_USERNAME);
+        boolean pass = (userPassword != null) && userPassword.matches(VALID_PASSWORD);
+        if (validName && pass) {
             request.setAttribute("userName", userName);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either UserName Or Password Is Incorrect</font>");
+            out.println("<font color=red>Enter Valid UserName Or Password</font>");
             requestDispatcher.include(request, response);
         }
     }
